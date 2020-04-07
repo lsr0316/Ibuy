9b5cca9dc277089443ec9ef5d584067276b099ca
Sudo-Si <56238070+Sudo-Si@users.noreply.github.com>
2020-04-08 11:36:28 +1200

    Mob App Added

--- Ibuy/Resources/Resource.designer.cs
+++ Ibuy/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace Ibuy
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
@@ -41,18 +41,27 @@ namespace Ibuy
 			}
 		}
 		
-		public partial class Id
+		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7F010000
+			public const int colorAccent = 2130771968;
 			
-			static Id()
+			// aapt resource value: 0x7F010001
+			public const int colorPrimary = 2130771969;
+			
+			// aapt resource value: 0x7F010002
+			public const int colorPrimaryDark = 2130771970;
+			
+			// aapt resource value: 0x7F010003
+			public const int ic_launcher_background = 2130771971;
+			
+			static Color()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
-			private Id()
+			private Color()
 			{
 			}
 		}
@@ -60,8 +69,8 @@ namespace Ibuy
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7F020000
+			public const int activity_main = 2130837504;
 			
 			static Layout()
 			{
@@ -76,8 +85,14 @@ namespace Ibuy
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7F030000
+			public const int ic_launcher = 2130903040;
+			
+			// aapt resource value: 0x7F030001
+			public const int ic_launcher_foreground = 2130903041;
+			
+			// aapt resource value: 0x7F030002
+			public const int ic_launcher_round = 2130903042;
 			
 			static Mipmap()
 			{
@@ -92,11 +107,11 @@ namespace Ibuy
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7F040000
+			public const int action_settings = 2130968576;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7F040001
+			public const int app_name = 2130968577;
 			
 			static String()
 			{
@@ -104,6 +119,22 @@ namespace Ibuy
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7F050000
+			public const int AppTheme = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
