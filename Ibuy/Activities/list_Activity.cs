77543df877777939e5283744e457e8f204da2e51
Shirui Liu <i1811398@nzse.ac.nz>
2020-04-13 16:31:54 +1200

    conetect the list

---
+++ Ibuy/Activities/list_Activity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+
+using Ibuy.Adapter;
+
+namespace Ibuy.Activities
+{
+    class list_Activity
+    {
+        RecyclerView mRecycleView;
+        RecyclerView.LayoutManager mLayoutManager;
+        PhotoAlbum mPhotoAlbum;
+        PhotoAlbumAdapter mAdapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            mPhotoAlbum = new PhotoAlbum();
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.list);
+            mRecycleView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecycleView.SetLayoutManager(mLayoutManager);
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            mAdapter.ItemClick += MAdapter_ItemClick;
+            mRecycleView.SetAdapter(mAdapter);
+        }
+
+        private void SetContentView(int list)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MAdapter_ItemClick(object sender, int e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Ibuy/Adapter/PhotoAlbumAdapter.cs
@@ -0,0 +1,45 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Ibuy.Activities;
+using System;
+using static Ibuy.Activities.PhotoAlbum;
+
+
+namespace Ibuy.Adapter
+{
+    class PhotoAlbumAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<int> ItemClick;
+        private PhotoAlbum mPhotoAlbum;
+        public PhotoAlbumAdapter(PhotoAlbum photoAlbum)
+        {
+            mPhotoAlbum = photoAlbum;
+        }
+        public override int ItemCount
+        {
+            get { return mPhotoAlbum.numPhoto; }
+        }
+
+        internal PhotoAlbum MPhotoAlbum { get => mPhotoAlbum; set => mPhotoAlbum = value; }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+
+            PhotoViewHolder vh = holder as PhotoViewHolder;
+            vh.Image.SetImageResource(mPhotoAlbum[position].mPhotoID);
+            vh.Caption.Text = mPhotoAlbum[position].mCaption;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.photocard, parent, false);
+            PhotoViewHolder vh = new PhotoViewHolder(itemView, OnClick);
+            return vh;
+        }
+        private void OnClick(int obj)
+        {
+            if (ItemClick != null)
+                ItemClick(this, obj);
+        }
+    }
+}
